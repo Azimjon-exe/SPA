b5d242928dcf23ef7c199eda7508c751ca97cf00
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2022-11-09 20:41:25 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <h1>SPA</h1>;
+  return (
+    <div>
+      <Header />
+      <main className="content">
+        <Route path="/" component={Home} />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="a" className="brand-logo">
+            Raect shop
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="a">REPO</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
